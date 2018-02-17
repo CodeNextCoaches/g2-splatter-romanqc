8822cbcb4995dfe4d217f7b6157230c9d335e9d9
Student <student@Admins-MacBook-Air.local>
2018-02-17 12:18:48 -0800

    update

--- script.js
+++ script.js
@@ -28,3 +28,63 @@
   *  x = random value between mouseX - spread and mouseX + spread
   *  y = random value between mouseY - spread and mouseY + spread
   */
+var canvas;
+var canvasArea;
+var r;
+var g;
+var b;
+
+function setup(){
+  canvas = createCanvas(600,500);
+  canvas.parent("#canvas-area");
+  canvas.mousePressed(drawEllipse);
+  background("#999999");
+  r = 255;
+  g = 0;
+  b = 0;
+}
+ function draw(){
+   fill(r,g,b);
+   ellipse(mouseX,mouseY,50,50);
+   
+   if(r == 255 && b == 0){
+      if(g <= 255){
+        g += 1;
+      }
+   }
+    if(g == 255 && b == 0){
+      if(r <= 255){
+        r -= 1;
+      }
+   }
+    if(r == 0 && g == 255){
+      if(b <= 255){
+        b += 1;
+      }
+   }
+    if(r == 0 && b == 255){
+      if(g <= 255){
+        g -= 1;
+      }
+   }
+    if(g <= 0 && b == 255){
+      if(r <= 255){
+        r += 1;
+      }
+   }
+    if(r == 255 && g == 0){
+      if(b <= 255){
+        b -= 1;
+      }
+   }
+
+ }
+
+
+function drawEllipse(){
+
+  fill(r,g,b);
+  noStroke();
+  ellipse(mouseX,mouseY,50,50);
+
+}
